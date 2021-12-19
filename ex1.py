c2546f548a843234b0552cbcd191184b665d92ab
sehong1 <96334729+sehong1@users.noreply.github.com>
2021-12-20 07:47:22 +0900

    Add files via upload

--- ex1.py
+++ ex1.py
@@ -34,7 +34,7 @@ is_ms = 0
 while True:
     try:
         temp = get_cci().astype(float)
-        time.sleep(1)
+        time.sleep(300)
         if temp is not None:
             a.append(temp)
         else:
@@ -42,13 +42,11 @@ while True:
 
         if len(a) > 5:
             a.__delitem__(0)
-            delta = a[-1] - a[-2]
-            print(a, delta)
-            if is_ms == 0 and sum(a[1:4])/4 < -100 and -100 < a[-1]:
+            if is_ms == 0 and a[-3] < -100 < a[-2]:
                 krw = get_balance("KRW")
                 upbit.buy_market_order("KRW-BTC", krw * 0.9995)
                 is_ms = 1
-            if is_ms == 1 and sum(a[1:4])/4 > 100 and a[-1] < 100:
+            if is_ms == 1 and a[-3] > 100 and a[-2] < 100:
                 btc = get_balance("BTC")
                 upbit.sell_market_order("KRW-BTC", btc * 0.9995)
                 is_ms = 0
