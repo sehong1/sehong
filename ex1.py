15967189c7331ed83d15bf1f5e9857b57eba2b05
sehong1 <96334729+sehong1@users.noreply.github.com>
2021-12-21 01:29:02 +0900

    Add files via upload

--- ex1.py
+++ ex1.py
@@ -10,8 +10,8 @@ upbit = pyupbit.Upbit(access, secret)
 def get_cci():
     df = pyupbit.get_ohlcv("KRW-BTC", interval="minute5")
     pt = (df['high'] + df['low'] + df['close']) / 3
-    sma = pt.rolling(20).mean()
-    mad = pt.rolling(20).apply(lambda x: pd.Series(x).mad())
+    sma = pt.rolling(14).mean()
+    mad = pt.rolling(14).apply(lambda x: pd.Series(x).mad())
     cci = (pt - sma) / (0.015 * mad)
     return cci[-3:]
 
@@ -30,20 +30,20 @@ def get_balance(ticker):
 
 while True:
     try:
-        temp = get_cci().astype(float)
         time.sleep(1)
+        temp = get_cci().astype(float)
         if temp is not None:
             a = temp
         else:
             pass
-        if sum(a[0:2]) / 2 < -100 < a[-1]:
+        if -150 > a[-1]:
             krw = get_balance("KRW")
             if krw > 5000:
-                upbit.buy_market_order("KRW-BTC", krw * 0.9995)
-        if sum(a[0:2]) / 2 > 100 > a[-2]:
+                upbit.buy_market_order("KRW-BTC", krw * 0.333)
+        if 150 < a[-1]:
             btc = get_balance("BTC")
             if btc > 0.00008:
-                upbit.sell_market_order("KRW-BTC", btc * 0.9995)
+                upbit.sell_market_order("KRW-BTC", btc * 0.333)
     except Exception as e:
         print(e)
         time.sleep(1)
